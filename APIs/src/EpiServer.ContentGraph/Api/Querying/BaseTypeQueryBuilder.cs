ce56b90d197f441b823572dd08bdec0ce8324b80
ManhOptimizely <manh.nguyen@optimizely.com>
2024-03-26 13:14:24 +0700

    FIND-12462: Support link type for Link query
    - New class LinkQueryBuilder use for build a link query
    - Add unit test

--- APIs/src/EpiServer.ContentGraph/Api/Querying/BaseTypeQueryBuilder.cs
+++ APIs/src/EpiServer.ContentGraph/Api/Querying/BaseTypeQueryBuilder.cs
@@ -64,22 +64,35 @@ namespace EPiServer.ContentGraph.Api.Querying
 
             return this;
         }
-        public virtual BaseTypeQueryBuilder Link(BaseTypeQueryBuilder link)
+        public virtual BaseTypeQueryBuilder Link(ITypeQueryBuilder link)
         {
             link.ValidateNotNullArgument("link");
-            string linkItems = link.GetQuery()?.Query ?? string.Empty;
-            if (!linkItems.IsNullOrEmpty())
+            var linkQueryBuilder = link as ILinkQueryBuilder;
+            if (linkQueryBuilder is null)
             {
+                throw new ArgumentException("The argument [link] is not type of [LinkQueryBuilder]");
+            }
+            string linkQuery = linkQueryBuilder.GetQuery()?.Query ?? string.Empty;
+            if (!linkQuery.IsNullOrEmpty())
+            {
+                if (!linkQueryBuilder.GetLinkType().IsNullOrEmpty())
+                {
+                    graphObject.SelectItems.Append(
+                        graphObject.SelectItems.Length == 0 ?
+                        $"_link(type:{linkQueryBuilder.GetLinkType()})" :
+                        $" _link(type:{linkQueryBuilder.GetLinkType()})"
+                    );
+                }
                 graphObject.SelectItems.Append(
                     graphObject.SelectItems.Length == 0 ?
-                    $"_link{{{linkItems}}}" :
-                    $" _link{{{linkItems}}}"
+                    $"{{{linkQuery}}}" :
+                    $" {{{linkQuery}}}"
                 );
             }
             return this;
         }
         [Obsolete("Use Link method instead")]
-        public virtual BaseTypeQueryBuilder Children(BaseTypeQueryBuilder children)
+        public virtual BaseTypeQueryBuilder Children(ITypeQueryBuilder children)
         {
             children.ValidateNotNullArgument("children");
             string childrenItems = children.GetQuery()?.Query ?? string.Empty;
